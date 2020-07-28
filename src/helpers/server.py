25e30b9c000445a2a19329b79cbf5ab6958a7b84
Nathan Botelho <nimbotelho@gmail.com>
2020-07-27 22:54:42 -0300

    refactor: create server environment

--- src/helpers/server.py
+++ src/helpers/server.py
@@ -1,19 +1,17 @@
-class Environment:
+class Server:
 
     __host = '127.0.0.1'
-
     __debug = True
-
     __port = 5000
 
     @staticmethod
     def getHost():
-        return Environment.__host
+        return Server.__host
 
     @staticmethod
     def getDebug():
-        return Environment.__debug
+        return Server.__debug
 
     @staticmethod
     def getPort():
-        return Environment.__port
+        return Server.__port
