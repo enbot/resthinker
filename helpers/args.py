12a1ffb5f4d670c3d340be56e7b045c1e6ee6a43
Nathan Botelho <nimbotelho@gmail.com>
2020-08-22 20:55:06 -0300

    feat: update args and emotion implementation

--- helpers/args.py
+++ helpers/args.py
@@ -11,13 +11,13 @@ class Args:
 
     uuid_help = "The uuid number to load an especific brain"
 
-    state_help = "The current state of emotion of response"
+    emotion_help = "The current emotion of response"
 
     def __init__(self):
         parser = argparse.ArgumentParser(description=self.description)
-        parser.add_argument('-m', '--message', action="store", dest="message", help=self.message_help, required=True)
-        parser.add_argument('-s', '--state', action="store", dest="state", help=self.state_help, required=True)
-        parser.add_argument('-u', '--uuid', action="store", dest="uuid", help=self.uuid_help, required=True)
+        parser.add_argument("-m", "--message", action="store", dest="message", help=self.message_help, required=True)
+        parser.add_argument("-e", "--emotion", action="store", dest="emotion", help=self.emotion_help, required=True)
+        parser.add_argument("-u", "--uuid", action="store", dest="uuid", help=self.uuid_help, required=True)
         self.__params = parser.parse_args()
 
     def getParams(self):
--- main.py
+++ main.py
@@ -6,13 +6,16 @@ core = Core()
 
 params = args.getParams()
 message = params.message
-state = params.state
+emotion = params.emotion
 uuid = params.uuid
 
-status = core.start(state, uuid)
-callback = core.ask(message)
-
-print('input >> ' + message)
-print('output >> ' + callback)
-print('status >> ' + status)
-print('uuid >> ' + uuid)
+try:
+    status = core.start(emotion, uuid)
+    response = core.ask(message)
+    print("uuid: " + uuid)
+    print("input: " + message)
+    print("status: " + status)
+    print("emotion: " + emotion)
+    print("response: " + response)
+except:
+    print("error: failed to process")
